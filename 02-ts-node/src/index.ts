8705530717a521a60a461b575c4eac3e5559be0a
adba98 <oscar.adba@gmail.com>
2022-10-29 10:48:27 -0500

    :sparkles: Add decorators

--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -1,6 +1,10 @@
-import { getPokemon } from './generics/get-pokemon';
+import { Pokemon } from './decorators/pokemon-class';
 
-getPokemon(4)
-  .then((pokemon) => console.log(pokemon))
-  .catch((error) => console.error(error))
-  .finally(() => console.log('Fin de getPokemon'));
+const charmander = new Pokemon('Charmander');
+
+// (Pokemon.prototype as any).customName = 'Pikachu'
+
+// console.log(charmander.savePokemonToDB(50));
+// charmander.savePokemonToDB(10);
+charmander.publicApi = 'https://my-api.com';
+console.log(charmander);
